5b93496f4ea5731e3c1a9cfbe9a7d4dec01cc2f6
Abraham <abrahamgil2@gmail.com>
2020-01-04 15:08:11 -0600

    updated function to take a list or dictionary and it will return the mean

--- basics.py
+++ basics.py
@@ -1,6 +1,11 @@
-def mean(mylist):
-    the_mean = sum(mylist) / len(mylist)
+def mean(value):
+    if type(value) == dict:
+        the_mean = sum(value.values()) / len(value)
+    else:
+        the_mean = sum(value) / len(value)
+
     return the_mean
 
-
-print(mean([1, 4, 6]))
+monday_temperatures = [8.8, 9.1, 9.9]
+student_grades = {"Marry": 9.1, "Sim": 8.8, "John": 7.5}
+print(mean(monday_temperatures))
