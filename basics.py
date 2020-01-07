4c34412798d0eace61355a6f860ca552c56b16e2
Abraham <abrahamgil2@gmail.com>
2020-01-07 13:44:02 -0600

    Manipulating User Input Results

--- basics.py
+++ basics.py
@@ -1,9 +1,4 @@
-def weather_condition(temperature):
-    if temperature > 7:
-        return "Warm"
-    else:
-        return "Cold"
-
-user_input = float(input("Enter temperature: "))
-
-print(weather_condition(user_input))
+user_input = input("Enter your name: ")
+message = "Hello %s!" % user_input
+message = f"Hello {user_input}"
+print(message)
