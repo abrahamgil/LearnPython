47b351654ce3d003b7556c80934372de80fc92a4
Abraham <abrahamgil2@gmail.com>
2020-01-08 14:29:18 -0600

    String formatting and multiple inputs

--- basics.py
+++ basics.py
@@ -1,4 +1,9 @@
-user_input = input("Enter your name: ")
-message = "Hello %s!" % user_input
-message = f"Hello {user_input}"
+#user_input = input("Enter your name: ")
+name = input("Enter your name: ")
+surname= input("Enter your surname: ")
+when = "today"
+
+message = "Hello %s %s!" % (name, surname)
+message = f"Hello {name} {surname}. What's up {when}"
 print(message)
+
