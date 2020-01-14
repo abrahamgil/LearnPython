1aef1e3f76b28814cf9b0dc78c9d4856ea210347
Abraham <abrahamgil2@gmail.com>
2020-01-14 07:13:41 -0600

    TextPro app first stage.

---
+++ textpro.py
@@ -0,0 +1,17 @@
+#First app to test fundamentals. 
+#Problem: Given the result provided in the GIF create the app. User continues inputs
+#         until user ends with /end. After the sentences are formatted to for a 
+#         clean output
+
+def saysomething():
+    while True:
+        user_input = input("Say something: ")
+        if user_input == "\end"
+            break
+        else
+            continue
+        final_statement
+
+def statementCleanUp(stuff):
+
+
