31adf88235c963fcc811534871840a38544d145b
Abraham <abrahamgil2@gmail.com>
2020-01-15 13:47:43 -0600

    Storing inputs into array

--- textpro.py
+++ textpro.py
@@ -3,15 +3,26 @@
 #         until user ends with /end. After the sentences are formatted to for a 
 #         clean output
 
+
 def saysomething():
+    i = 1
+    final_statement = [0,1,2,3,4,5,6,7,8,9]
     while True:
+        
         user_input = input("Say something: ")
-        if user_input == "\end"
+        if user_input == "\end":
             break
-        else
+        else:
+            final_statement[i] = user_input
+            print(final_statement[i])
+            i = i + 1
             continue
-        final_statement
+        
+        
+
+        
 
 def statementCleanUp(stuff):
+    return stuff
 
-
+saysomething()
