5b9705f5f9214668a2d1e9c439d865368df9ed9a
Abraham <abrahamgil2@gmail.com>
2020-01-22 05:44:15 -0600

    adding conditionals

--- basics.py
+++ basics.py
@@ -1,4 +1,4 @@
-""" iteration / recursive
+""" recursive
 temps = [221, 234, 340, 230]
 
 new_temps = []
@@ -9,9 +9,9 @@ for temp in temps:
 print(new_temps)
 """
 
-#
-temps = [221, 234, 340, 230]
+#list comprehension and dynamically update in array cleaner
+temps = [221, 234, 340, -9999, 230]
 
-new_temps = [temp / 10 for temp in temps]
+new_temps = [temp / 10 for temp in temps if temp != -9999]
 
 print(new_temps)
