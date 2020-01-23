7aeb70a12e4f23b885317a9bfc601bf95de5c83d
Abraham <abrahamgil2@gmail.com>
2020-01-23 14:40:42 -0600

    Building functions with multiple arguments

--- basics.py
+++ basics.py
@@ -1,17 +1,4 @@
-""" recursive
-temps = [221, 234, 340, 230]
+def area(a, b):
+    return a * b
 
-new_temps = []
-for temp in temps:
-    new_temps.append(temp / 10)
-
-
-print(new_temps)
-"""
-
-#list comprehension and dynamically update in array cleaner
-temps = [221, 234, 340, -9999, 230]
-
-new_temps = [temp / 10 for temp in temps if temp != -9999]
-
-print(new_temps)
+print(area(4,5))
