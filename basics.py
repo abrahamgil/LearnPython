94eb696c9511e1e31060d0e463eae5aa5c7468a4
Abraham <abrahamgil2@gmail.com>
2020-01-24 03:09:56 -0600

    Create function that takes multiple strings and manipulates the strings but spits out a list

--- basics.py
+++ basics.py
@@ -1,5 +1,11 @@
 def mean(*args):
     return sum(args) / len(args)
 
+def foo(*args):
+    new_list = [i.upper() for i in args]
+    new_list.sort()
+    return new_list
 
 print((mean(1,3,4)))
+
+print(foo('sup', 'how', 'are', 'damn'))
