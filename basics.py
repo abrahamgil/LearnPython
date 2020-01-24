186749bcd08e0eb0a4cbef4b935dc5b68b7aca90
Abraham <abrahamgil2@gmail.com>
2020-01-24 03:19:23 -0600

    Learning to read files and spit out the contents

--- basics.py
+++ basics.py
@@ -1,11 +1,2 @@
-def mean(*args):
-    return sum(args) / len(args)
-
-def foo(*args):
-    new_list = [i.upper() for i in args]
-    new_list.sort()
-    return new_list
-
-print((mean(1,3,4)))
-
-print(foo('sup', 'how', 'are', 'damn'))
+myfile = open("fruits.txt")
+print(myfile.read())
