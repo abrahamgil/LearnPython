a12d7c062707f80fdfdc56cb6034a57ff4c86f6e
Abraham <abrahamgil2@gmail.com>
2020-01-29 06:55:20 -0600

    Added Pandas library and used it to calculate mean from data in csv

--- basics.py
+++ basics.py
@@ -1,11 +1,11 @@
 import time
 import os
+import pandas
 
 while True:
-    if os.path.exists("files/vegetables.txt"):
-        with open("files/vegetables.txt") as file:
-            print(file.read())
-            
+    if os.path.exists("files/temps-today.csv"):
+        data = pandas.read_csv("files/temps-today.csv")
+        print(data.mean()["st1"])       
     else:
         print("File does not exist.")
     time.sleep(10)
